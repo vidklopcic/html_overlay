e6969cd9a988dd880b67ce4935157cbee005e3ce
vidklopcic <klopcic.vid@gmail.com>
2021-08-13 12:17:44 +0200

    refactoring

--- test/html_overlay_test.dart
+++ test/html_overlay_test.dart
@@ -16,8 +16,4 @@ void main() {
   tearDown(() {
     channel.setMockMethodCallHandler(null);
   });
-
-  test('getPlatformVersion', () async {
-    expect(await HtmlOverlay.platformVersion, '42');
-  });
 }
